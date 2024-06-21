fd12b2aae46c20bd5a91d099344bd18884faea34
MrCl0wnLab <0x27null@gmail.com>
2024-06-21 01:08:14 -0300

    Implementação de Banner x Arquivos asciiart

--- core/filelocal.py
+++ core/filelocal.py
@@ -1,10 +1,12 @@
 import csv
+import pathlib
 from core.style_cli import StyleCli
 
 
 class FileLocal:
     def __init__(self):
         self.__cli = StyleCli()
+        self.file_skipe = None
 
     def open_file(self, filename: str, mode: str):
         if filename and mode:
@@ -15,6 +17,8 @@ class FileLocal:
             except FileNotFoundError:
                 self.__cli.console.print_exception(max_frames=3)
             except PermissionError:
+                self.__cli.console.print_exception(max_frames=3)
+            except Exception:
                 self.__cli.console.print_exception(max_frames=3)
             finally:
                 data_return.close()
@@ -28,8 +32,7 @@ class FileLocal:
             except IOError:
                 self.__cli.console.print_exception(max_frames=3)
 
-
-    def open_fileCsv(self, filename: str, mode: str):
+    def open_file_csv(self, filename: str, mode: str):
         if filename and mode:
             try:
                 # TODO
@@ -39,4 +42,22 @@ class FileLocal:
                 csv_line = data_file
                 return csv_line, data_return
             except IOError:
+                self.__cli.console.print_exception(max_frames=3)
+
+    def list_file_dir(self, dir_str: str) -> dict[list[str], list[str] | str]:
+        dir_list: list = []
+        file_list: list = []
+        skipe_list: list = ["etc", "bin", "home"]
+        if dir_str:
+            try:
+                obj_path = pathlib.Path(dir_str)
+                path_tree_list = list(obj_path.rglob("*"))
+                for dir_file in path_tree_list:
+                    if set(dir_file.parts).isdisjoint(skipe_list):
+                        if dir_file.is_dir():
+                            dir_list.append(dir_file)
+                        elif dir_file.is_file():
+                            file_list.append(dir_file)
+                return {'dirs': dir_list, 'files': file_list}
+            except Exception:
                 self.__cli.console.print_exception(max_frames=3)
